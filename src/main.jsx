bfa399beaf5383092545adc0a0b9be169221cd1d
Sergii Antonenko <gerbar50@gmail.com>
2024-07-18 16:40:41 +0300

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "components/App.jsx";
+import { App } from "./components/App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/project-DigitalTeam">
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/project-DigitalTeam">
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
