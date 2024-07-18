0cd4c315bc82ae844b5ecb08889a19510fa2a6d8
Sergii Antonenko <gerbar50@gmail.com>
2024-07-18 16:22:05 +0300

    upd config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         fontSize: 40,
-        color: '#010101'
+        color: "#010101",
       }}
     >
-      React homework template
+      React Node Project
     </div>
   );
 };
