e495122c05e8342b1ab7b54e87822d36543082dd
YuliyaRoslikova <julia.roslikov@gmail.com>
2024-07-21 01:24:01 +0300

    redux-draft

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,51 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { api } from '../../api/api';
+
+export const register = createAsyncThunk('auth/register', async (credentials, thunkAPI) => {
+  try {
+    const response = await api.instance.post('auth/register', credentials);
+
+    api.setAuthHeader(response.data.token);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const logIn = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
+  try {
+    const res = await api.instance.post('auth/login', credentials);
+
+    api.setAuthHeader(res.data.token);
+    return res.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await api.instance.post('auth/logout');
+
+    api.clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk('auth/refresh-token', async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const persistedToken = state.auth.token;
+
+  if (persistedToken === null) {
+    return thunkAPI.rejectWithValue('Unable to fetch user');
+  }
+
+  try {
+    api.setAuthHeader(persistedToken);
+    const res = await api.instance.get('users/current');
+    return res.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
---
+++ src/redux/water/selectors.js
@@ -0,0 +1,14 @@
+// import { createSelector } from '@reduxjs/toolkit';
+// import { selectNameFilter } from '../filters/selectors';
+
+// export const selectContacts = state => state.contacts.items;
+
+// export const selectContactsError = state => state.contacts.error;
+
+// export const selectContactsLoading = state => state.contacts.loading;
+
+// export const selectFilteredContacts = createSelector(
+//   [selectContacts, selectNameFilter],
+//   (contacts, filter) =>
+//     contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+// );
