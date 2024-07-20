5c14cad2862c006eb1a301359a9395a8084adefe
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-07-20 17:26:41 +0400

    editwatermodal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import ModalAddWater from './components/Modals/ModalAddWater';
-
+import ModalAddWater from './components/Modals/ModalAdd/ModalAddWater';
+import ModalEditWater from './components/Modals/EditWater/EditWater';
 
 export const App = () => {
   return <div>
-
+<ModalEditWater/>
     <ModalAddWater/>
     React Node Project
   </div>;
