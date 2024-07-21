6a03ba5dc5684702400307ae065cd70b11a17e48
Dmytro Maniuta <ip722428@gmail.com>
2024-07-21 13:24:07 +0300

    merged two pages WaterMainInfo and WaterDetailedInfo together

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -4,20 +4,24 @@ import WaterDailyNorma from './WaterDailyNorma/WaterDailyNorma';
 import css from './WaterMainInfo.module.css';
 
 export const Logo = () => {
-    return (
-        <Link className={css.logo} to="/">AquaTrack</Link>
-    );
+  return (
+    <Link className={css.logo} to="/">
+      AquaTrack
+    </Link>
+  );
 };
 
 const WaterMainInfo = () => {
-    return (
-        <div className={css.waterMainInfo}>
-            <div className={css.logoAT}><Logo/></div>
-            
-            <WaterDailyNorma />
-            <AddWaterBtn />
-        </div>
-    )
-}
+  return (
+    <div className={css.waterMainInfo}>
+      <div className={css.logoAT}>
+        <Logo />
+      </div>
+
+      <WaterDailyNorma />
+      <AddWaterBtn />
+    </div>
+  );
+};
 
 export default WaterMainInfo;
