b8f7c9df2a8fe183b5d8ec667650ef9537e246df
Vadym <vadymlysytskyi@gmail.com>
2024-07-21 19:30:11 +0200

    fix routes in navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { App } from './App.jsx';
 import './index.css';
 import { store, persistor } from './redux/store';
@@ -10,14 +10,14 @@ import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store}> */}
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </PersistGate>
+    {/* </Provider> */}
   </React.StrictMode>
 );
