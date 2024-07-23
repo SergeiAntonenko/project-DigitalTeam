d6c237ada81e3ff35bd5c0d77a350c7416c2d017
Sergii Antonenko <gerbar50@gmail.com>
2024-07-23 19:28:42 +0300

    upd home page

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,10 +1,11 @@
+import Logo from '../Logo/Logo.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 import css from './WelcomeSection.module.css';
 
 const WelcomeSection = () => {
   return (
     <div className={css.welcomeSection}>
-      <h3 className={css.logo}>AquaTrack</h3>
+      <Logo />
       <div className={css.tabletWrapper}>
         <p className={css.descr}>Record daily water intake and track</p>
         <h1 className={css.water}>Water consumption tracker</h1>
