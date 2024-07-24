7e289d10df6e71f7bd46cb1da938053c252d9390
SergeiAntonenko <110186592+SergeiAntonenko@users.noreply.github.com>
2024-07-24 21:38:08 +0300

    Update CustomersPhoto.jsx

--- src/components/CustomersPhoto/CustomersPhoto.jsx
+++ src/components/CustomersPhoto/CustomersPhoto.jsx
@@ -1,7 +1,4 @@
 import css from '../CustomersPhoto/CustomersPhoto.module.css';
-// import firstFemale from '../../photos/HomePage-images/First-Female-min.jpg';
-// import male from '../../photos/HomePage-images/Male-min.jpg';
-// import lastFemale from '../../photos/HomePage-images/Last-Female-min.jpg';
 
 const CustomersPhoto = () => {
   return (
