2430b4ecb618718d71aa718ac04dcb1f1d90c731
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-07-24 14:14:52 +0400

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from './components/Layout/Layout.jsx';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import Logout from './components/Modals/LogOutModal.jsx/LogOutModal.jsx';
-import ModalDelete from './components/Modals/ModalDelete/ModalDelete.jsx'
-import  ModalEditWater from './components/Modals/EditWater/EditWater.jsx'
 // import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
 const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage.jsx'));
@@ -14,11 +11,7 @@ const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage.jsx'));
 export const App = () => {
   return (
     <div>
-
-      <Logout />
-      <ModalDelete />
-      < ModalEditWater/>
-      <Layout>
+        <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
           {/* <Route path="/" element={<RestrictedRoute redirectTo="/tracker" component={<HomePage />} />} /> */}
