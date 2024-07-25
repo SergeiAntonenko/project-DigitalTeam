26377f730fd83fbf1c4e4dff25121b5289b5a773
YuliyaRoslikova <julia.roslikov@gmail.com>
2024-07-26 02:08:15 +0300

    init localization

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,14 +1,20 @@
+import LanguageToggle from '../LanguageToggle/LanguageToggle.jsx';
 import Logo from '../Logo/Logo.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 import css from './WelcomeSection.module.css';
+import { useTranslation } from 'react-i18next';
 
 const WelcomeSection = () => {
+  const { t } = useTranslation();
   return (
     <div className={css.welcomeSection}>
-      <Logo />
+      <div className={css.container}>
+        <Logo />
+        <LanguageToggle />
+      </div>
       <div className={css.tabletWrapper}>
-        <p className={css.descr}>Record daily water intake and track</p>
-        <h1 className={css.water}>Water consumption tracker</h1>
+        <p className={css.descr}>{t('welcome-section.description')}</p>
+        <h1 className={css.water}>{t('welcome-section.title')}</h1>
         <Navigation />
       </div>
     </div>
