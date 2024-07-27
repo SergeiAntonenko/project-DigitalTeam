007656a7394110e940f6617c609b991e2069f7e4
Dmytro Maniuta <ip722428@gmail.com>
2024-07-27 15:31:59 +0300

    added open and close  modal window UserSettingsModal, upd Today

--- src/components/MonthInfo/CalendarPagination/CalendarPagination.jsx
+++ src/components/MonthInfo/CalendarPagination/CalendarPagination.jsx
@@ -1,17 +1,64 @@
-import { format, subMonths, addMonths } from 'date-fns';
+// import { format, subMonths, addMonths } from 'date-fns';
+// import styles from './CalendarPagination.module.css';
+// import leftChevron from '../../../images/AdvantagesSection/chevron-left.svg';
+// import rightChevron from '../../../images/AdvantagesSection/chevron-right.svg';
+
+// const CalendarPagination = ({ currentDate, onDateChange }) => {
+//   const handlePreviousMonth = () => {
+//     const newDate = subMonths(currentDate, 1);
+//     onDateChange(newDate);
+//   };
+
+//   const handleNextMonth = () => {
+//     const newDate = addMonths(currentDate, 1);
+//     onDateChange(newDate);
+//   };
+
+//   return (
+//     <div className={styles.container}>
+//       <button onClick={handlePreviousMonth} className={styles.navButton}>
+//         <img src={leftChevron} alt="Next Month" />
+//       </button>
+//       <span className={styles.month}>
+//         {currentDate ? format(currentDate, 'MMMM, yyyy') : 'Invalid Date'}
+//       </span>
+//       <button onClick={handleNextMonth} className={styles.navButton}>
+//         <img src={rightChevron} alt="Next Month" />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default CalendarPagination;
+// ================================================================
+import { format, subMonths, addMonths, parse, startOfMonth } from 'date-fns';
 import styles from './CalendarPagination.module.css';
 import leftChevron from '../../../images/AdvantagesSection/chevron-left.svg';
 import rightChevron from '../../../images/AdvantagesSection/chevron-right.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectMonth, setMonth } from '../../../redux/date/dateSlice';
 
 const CalendarPagination = ({ currentDate, onDateChange }) => {
+  const dispatch = useDispatch();
+  const date = useSelector(selectMonth);
+  const parsedDate = parse(date, 'MM-yyyy', new Date());
+
   const handlePreviousMonth = () => {
     const newDate = subMonths(currentDate, 1);
     onDateChange(newDate);
+    const prevoiusMonth = startOfMonth(
+      new Date(parsedDate.getFullYear(), parsedDate.getMonth() - 1, 1)
+    );
+    const month = format(prevoiusMonth, 'MM-yyyy');
+    dispatch(setMonth(month));
   };
 
   const handleNextMonth = () => {
     const newDate = addMonths(currentDate, 1);
     onDateChange(newDate);
+    const nextMonth = new Date(parsedDate.getFullYear(), parsedDate.getMonth() + 1, 1);
+    const month = format(nextMonth, 'MM-yyyy');
+    dispatch(setMonth(month));
   };
 
   return (
--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -2,11 +2,11 @@ import css from './UseBarPopover.module.css';
 import { IoSettingsOutline } from 'react-icons/io5';
 import { FiLogOut } from 'react-icons/fi';
 
-const UserBarPopover = ({ handleOpenModal }) => {
+const UserBarPopover = ({ handleOpenModal, handleOpenModal2 }) => {
   return (
     <div className={css.modalWrapp}>
       <div className={css.div}>
-        <button type="button" className={css.button}>
+        <button type="button" className={css.button} onClick={handleOpenModal2}>
           <IoSettingsOutline className={css.icon} />
           <span className={css.settings}>Setting</span>
         </button>
