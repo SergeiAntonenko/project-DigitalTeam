f4520c43f4514d19832ee2edfa24549375345cb6
Sergii Antonenko <gerbar50@gmail.com>
2024-07-29 08:55:00 +0300

    upd host

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true;
 class Api {
   constructor() {
     this.instance = axios.create({
-      baseURL: 'http://localhost:3000',
+      baseURL: 'https://project-digitalteam-backend.onrender.com',
       withCredentials: true,
     });
   }
