98ad11976457d1d6bcb2ee0f4837fe9d3eb8be9b
YuliyaRoslikova <julia.roslikov@gmail.com>
2024-07-30 09:52:13 +0300

    some fixes

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -22,26 +22,3 @@ const UserBarPopover = ({ handleOpenModal, handleOpenModal2 }) => {
 };
 
 export default UserBarPopover;
-
-// import css from './UseBarPopover.module.css';
-// import { IoSettingsOutline } from 'react-icons/io5';
-// import { FiLogOut } from 'react-icons/fi';
-
-// const UserBarPopover = ({ handleOpenModal, handleOpenSettingsModal }) => {
-//   return (
-//     <div className={css.modalWrapp}>
-//       <div className={css.div}>
-//         <button type="button" className={css.button} onClick={handleOpenSettingsModal}>
-//           <IoSettingsOutline className={css.icon} />
-//           <span className={css.settings}>Setting</span>
-//         </button>
-//         <button type="button" className={css.button} onClick={handleOpenModal}>
-//           <FiLogOut className={`${css.logOut} ${css.icon}`} />
-//           <span className={`${css.logOut} ${css.settings}`}>Log out</span>
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UserBarPopover;
--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -1,3 +1,37 @@
+// import React, { useEffect } from 'react';
+// import s from './Modal.module.css';
+// import Iconsvg from '../../../images/Icons/Icons';
+
+// const Modal = ({ children, handleCloseModal }) => {
+//   const handleEscapeKey = e => {
+//     if (e.key === 'Escape') {
+//       handleCloseModal();
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener('keydown', handleEscapeKey);
+//     document.body.style.overflow = 'hidden';
+//     return () => {
+//       document.removeEventListener('keydown', handleEscapeKey);
+//       document.body.style.overflow = 'auto';
+//     };
+//   }, []);
+
+//   return (
+//     <section onClick={handleCloseModal} className={s.backdrop}>
+//       <div className={s.container} onClick={e => e.stopPropagation()}>
+//         <button className={s.closeButton} onClick={handleCloseModal}>
+//           <Iconsvg className={s.closeIcon} iconName="icon-close" />
+//         </button>
+//         {children}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Modal;
+
 import React, { useEffect } from 'react';
 import s from './Modal.module.css';
 import Iconsvg from '../../../images/Icons/Icons';
