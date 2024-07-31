3ff776b04866af5a6e209c0a56b8e80fff098b0a
Sergii Antonenko <gerbar50@gmail.com>
2024-07-31 14:28:34 +0300

    upd

--- src/components/WaterMainInfo/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterMainInfo/WaterDailyNorma/WaterDailyNorma.jsx
@@ -8,7 +8,7 @@ const WaterDailyNorma = () => {
   const user = useSelector(selectUser);
 
   let dailyNorma = 1.5;
-  if (user.dailyWaterGoal !== null) {
+  if (user.dailyWaterGoal !== 'null') {
     dailyNorma = user.dailyWaterGoal;
   }
 
