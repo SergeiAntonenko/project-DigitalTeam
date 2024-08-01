733f74109276abd07f1cab4bf0a7c5e0caaf0816
Sergii Antonenko <gerbar50@gmail.com>
2024-08-01 12:01:34 +0300

    upd avatar

--- src/components/UserPanelAvatar/UserPanelAvatar.jsx
+++ src/components/UserPanelAvatar/UserPanelAvatar.jsx
@@ -1,10 +1,21 @@
 import s from './UserPanelAvatar.module.css';
 import defaultAvatar from './Rectangle22x-min.png';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../redux/users/selectors';
+import { useEffect, useState } from 'react';
 
 const UserPanelAvatar = () => {
+  const user = useSelector(selectUser);
+  const avatarURL = user.avatar;
+  const [preview, setPreview] = useState(avatarURL || defaultAvatar);
+
+  useEffect(() => {
+    setPreview(avatarURL || defaultAvatar);
+  }, [avatarURL]);
+
   return (
     <div className={s.thumb}>
-      <img src={defaultAvatar} alt="User avatar" className={s.img} />
+      <img src={preview} alt="User avatar" className={s.img} />
     </div>
   );
 };
