cdbb2e80ba09134c903747a0506192df5c613929
Eduard Venidiktov <edven7777777@gmail.com>
2024-08-02 16:25:27 +0300

    add toster if date not choose

--- src/components/MonthInfo/Statistics/Statistics.jsx
+++ src/components/MonthInfo/Statistics/Statistics.jsx
@@ -1,147 +1,3 @@
-// import { useState, useEffect, useRef } from 'react';
-// import { useSelector } from 'react-redux';
-// import { AreaChart, Area, XAxis, Tooltip, YAxis } from 'recharts';
-// import { curveCardinal } from 'd3-shape';
-// import moment from 'moment';
-// import styles from './Statistics.module.css';
-// import CustomTooltip from '../CustomTooltip/CustomTooltip';
-// import { selectTotalForAllDays } from '../../../redux/water/selectors';
-
-// const useMediaQuery = query => {
-//   const [matches, setMatches] = useState(false);
-
-//   useEffect(() => {
-//     const mediaQueryList = window.matchMedia(query);
-//     const documentChangeHandler = () => setMatches(mediaQueryList.matches);
-
-//     mediaQueryList.addEventListener('change', documentChangeHandler);
-//     documentChangeHandler(); // Initialization value
-
-//     return () => mediaQueryList.removeEventListener('change', documentChangeHandler);
-//   }, [query]);
-
-//   return matches;
-// };
-
-// const getData = (allDaysData, selectedDate) => {
-//   const data = [];
-//   const selected = moment(selectedDate, 'YYYY-MM-DD');
-//   const endDate = selected.clone().subtract(8, 'days');
-
-//   let date = selected;
-//   while (date.isAfter(endDate)) {
-//     const formattedDate = date.format('DD.MM.YYYY');
-//     const water = allDaysData[formattedDate] || 0;
-
-//     data.push({
-//       date: date.format('DD'),
-//       water,
-//     });
-//     date = date.clone().subtract(1, 'day');
-//   }
-
-//   return data.reverse();
-// };
-
-// const Statistics = ({ selectedDate }) => {
-//   const allDaysData = useSelector(selectTotalForAllDays);
-//   const data = getData(allDaysData, selectedDate);
-//   const cardinal = curveCardinal.tension(0.2);
-//   const scrollContainerRef = useRef(null);
-
-//   const isMobile = useMediaQuery('(max-width: 767px)');
-//   const chartWidth = isMobile ? data.length * 35 : data.length * 84;
-
-//   useEffect(() => {
-//     if (scrollContainerRef.current) {
-//       const container = scrollContainerRef.current;
-//       const scrollToEnd = () => {
-//         container.scrollTo({
-//           left: container.scrollWidth,
-//           behavior: 'smooth', // smooth scroll
-//         });
-//       };
-
-//       // Check, if wight > 767px, else scrolling to end
-//       if (window.innerWidth > 767) {
-//         scrollToEnd();
-//       } else {
-//         // For mobile simple scroll to end
-//         container.scrollLeft = container.scrollWidth;
-//       }
-//     }
-//   }, [data]);
-
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.yAxisContainer}>
-//         <AreaChart
-//           width={60}
-//           height={215}
-//           data={data}
-//           margin={{
-//             top: 45,
-//             right: 0,
-//             left: -15,
-//             bottom: 0,
-//           }}
-//           className={styles.chartWrapper}
-//         >
-//           <YAxis
-//             type="number"
-//             ticks={[0, 0.5, 1, 1.5, 2, 2.5]}
-//             tickFormatter={value => (value === 0 ? '0%' : `${value}L`)}
-//             axisLine={false}
-//             tickLine={false}
-//             className={styles.ySymbol}
-//             padding={{ bottom: 30 }}
-//           />
-//         </AreaChart>
-//       </div>
-//       <div className={styles.scrollContainer} ref={scrollContainerRef}>
-//         <AreaChart
-//           width={chartWidth}
-//           height={215}
-//           data={data}
-//           margin={{
-//             top: 50,
-//             right: 0,
-//             left: 40,
-//             bottom: 0,
-//           }}
-//           className={styles.chartWrapper}
-//         >
-//           <defs>
-//             <linearGradient id="colorWater" x1="0" y1="0" x2="0" y2="1">
-//               <stop offset="1.54%" className={styles.gradientStopTop} />
-//               <stop offset="93.64%" className={styles.gradientStopBottom} />
-//             </linearGradient>
-//           </defs>
-
-//           <XAxis
-//             dataKey="date"
-//             axisLine={false}
-//             tickLine={false}
-//             className={styles.xSymbol}
-//             padding={{ bottom: 30 }}
-//           />
-//           <Tooltip content={<CustomTooltip />} />
-//           <Area
-//             className={styles.area}
-//             type={cardinal}
-//             stroke="#87d28d"
-//             dataKey="water"
-//             fill="url(#colorWater)"
-//             dot={{ className: styles.dot }}
-//           />
-//         </AreaChart>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Statistics;
-
 import { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { AreaChart, Area, XAxis, Tooltip, YAxis } from 'recharts';
@@ -150,6 +6,8 @@ import moment from 'moment';
 import styles from './Statistics.module.css';
 import CustomTooltip from '../CustomTooltip/CustomTooltip';
 import { selectTotalForAllDays } from '../../../redux/water/selectors';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const useMediaQuery = query => {
   const [matches, setMatches] = useState(false);
@@ -172,8 +30,6 @@ const getData = (allDaysData, selectedDate) => {
   const selected = moment(selectedDate, 'YYYY-MM-DD');
   const startDate = selected.clone().startOf('month');
 
-  // const endDate = selected.clone().subtract(8, 'days');
-
   let date = selected;
   while (date.isSameOrAfter(startDate)) {
     const formattedDate = date.format('DD.MM.YYYY');
@@ -191,35 +47,48 @@ const getData = (allDaysData, selectedDate) => {
 
 const Statistics = ({ selectedDate }) => {
   const allDaysData = useSelector(selectTotalForAllDays);
-  const data = getData(allDaysData, selectedDate);
-  const cardinal = curveCardinal.tension(0.2);
   const scrollContainerRef = useRef(null);
-
   const isMobile = useMediaQuery('(max-width: 767px)');
+
+  useEffect(() => {
+    if (!selectedDate) {
+      toast.error('Choose any date');
+    }
+  }, [selectedDate]);
+
+  const data = selectedDate ? getData(allDaysData, selectedDate) : [];
+  const cardinal = curveCardinal.tension(0.2);
   const chartWidth = isMobile ? data.length * 35 : data.length * 84;
 
   useEffect(() => {
-    if (scrollContainerRef.current) {
+    if (scrollContainerRef.current && data.length > 0) {
       const container = scrollContainerRef.current;
       const scrollToEnd = () => {
         container.scrollTo({
           left: container.scrollWidth,
-          behavior: 'smooth', // smooth scroll
+          behavior: 'smooth',
         });
       };
 
-      // Check, if width > 767px, else scrolling to end
       if (window.innerWidth > 767) {
         scrollToEnd();
       } else {
-        // For mobile simple scroll to end
         container.scrollLeft = container.scrollWidth;
       }
     }
   }, [data]);
 
+  if (!selectedDate) {
+    return (
+      <>
+        <ToastContainer />
+      </>
+    );
+  }
+
   return (
     <div className={styles.container}>
+      <ToastContainer className={styles.toastContainer} />
       <div className={styles.yAxisContainer}>
         <AreaChart
           width={60}
